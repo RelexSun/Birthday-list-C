04de0cd412e7f7f46e0e6a1bb70f7cd0a6a72399
Lex <lex@Lexs-MacBook-Pro.local>
2024-07-12 00:19:27 +0700

    fix bug

--- include/function.h
+++ include/function.h
@@ -6,52 +6,52 @@
 const int NAME_LENGTH = 50;
 const char FILE_NAME[50] = "Birthday_list.txt";
 
-typedef struct {
+  struct Birthday{
   char name[NAME_LENGTH];
   int day, month, year;
 
-} Birthday;
+} b;
 
-// int fileValidation () {}
+void fileValidation (FILE *file) {
+  if (file == NULL) {
+      printf("Can't find file!!!");
+      exit(0);
+  }
+}
 
 void createBirthday() {
   system("clear");
   FILE *file = fopen(FILE_NAME, "a");
   int choice;
-  if (file == NULL) {
-    printf("Can't open file!!!");
-    exit(0);
-  }
-  Birthday b;
+
+  fileValidation(&file);
+
   do {
   printf("Enter name: ");
   scanf("%s", b.name);
   do {
   printf("Enter Date of Birth (dd/mm/yy): ");
   scanf("%2d/%2d/%4d", &b.day, &b.month, &b.year);
-    if (b.day <= 31 && b.month <= 12) {
+    if ((b.day <= 31 && b.month <= 12) && (b.day > 0 && b.month > 0)) {
       break;
     } else {
-      printf("Please make sure the Date of Birth is valid.\n");
+      printf("Date of Birth is invalid.❌\n");
     }
   } while (1);
   fprintf(file, "%s %d %d %d\n", b.name, b.day, b.month, b.year);
-  printf("Birthday added successfully!!!\n");
+  printf("Birthday added successfully!!!✅\n");
   printf("Press 1 to continue/ 0 to stop: ");
   scanf("%d", &choice);
   } while (choice != 0);
   fclose(file);
 }
+
 void readBirthday() {
   system("clear");
   FILE *file = fopen(FILE_NAME, "r");
 
-    if (file == NULL) {
-    printf("Can't open file!!!");
-    exit(0);
-  }
+  fileValidation(&file);
 
-  Birthday b;
   printf("\t\t\t**Birthday List**\n");
   printf("\n");
   printf("\t\t%-30s\tDD/MM/YYYY\n", "NAME");
@@ -64,33 +64,30 @@ void readBirthday() {
 
 void updateBirthday() {
   system("clear");
-  Birthday b;
+
   char name[NAME_LENGTH];
   int choice; 
   int found = 0;
 
   do {
     FILE *file = fopen(FILE_NAME, "r");
-    if (file == NULL) {
-      printf("Can't find file!!!");
-      exit(0);
-    }
+    fileValidation(&file);
     FILE *tempFile = fopen("temp.txt", "w");
-    if (tempFile == NULL) {
-      printf("Can't find file!!!");
-      exit(0);
-    }
+    fileValidation(&tempFile);
     printf("Enter the name of the birthday to update: ");
     scanf("%s", name);
     while (fscanf(file, "%s %d %d %d", b.name, &b.day, &b.month, &b.year) != EOF) {
     if (strcmp(b.name, name) == 0) {
+      do {
       found = 1;
-      printf("Enter day: ");
-      scanf("%d", &b.day);
-      printf("Enter month: ");
-      scanf("%d", &b.month);
-      printf("Enter year: ");
-      scanf("%d", &b.year);
+      printf("Enter Date of Birth tto update (dd/mm/yy): ");
+      scanf("%2d/%2d/%4d", &b.day, &b.month, &b.year);
+      if ((b.day <= 31 && b.month <= 12) && (b.day > 0 && b.month > 0)) {
+        break;
+      } else {
+        printf("Date of Birth is invalid.❌\n");
+      }
+  } while (1);
     }
     fprintf(tempFile, "%s %d %d %d\n", b.name, b.day, b.month, b.year);
   }
@@ -101,7 +98,7 @@ void updateBirthday() {
   } else {
     remove(FILE_NAME);
     rename("temp.txt", FILE_NAME);
-    printf("Birthday updated!!!\n");
+    printf("Birthday updated!!!✅\n");
   }
   printf("Press 1 to continue/ 0 to stop: ");
   scanf("%d", &choice);
@@ -113,23 +110,17 @@ void updateBirthday() {
 
 void deleteBirthday() {
   system("clear");
-  Birthday b;
+
   char name[NAME_LENGTH];
   int found = 0;
   int choice; 
 
   do {
-      FILE *file = fopen(FILE_NAME, "r");
-      if (file == NULL) {
-        printf("Can't find file!!!");
-        exit(0);
-    }
+    FILE *file = fopen(FILE_NAME, "r");
+    fileValidation(&file);
 
     FILE *tempFile = fopen("temp.txt", "w");
-    if (tempFile == NULL) {
-      printf("Can't find file!!!");
-      exit(0);
-    }
+    fileValidation(&tempFile);
     printf("Enter the name of the birthday to delete: ");
     scanf("%s", name);
     while (fscanf(file, "%s %d %d %d", b.name, &b.day, &b.month, &b.year) != EOF) {
@@ -145,10 +136,10 @@ void deleteBirthday() {
   if (found) {
     remove(FILE_NAME);
     rename("temp.txt", FILE_NAME);
-    printf("Birthday deleted!!!\n");
+    printf("Birthday deleted!!!✅\n");
 
   } else {
-    printf("Data not found!!!\n");
+    printf("Data not found!!!❌\n");
     remove("temp.txt");
   }
   printf("Press 1 to continue/ 0 to stop: ");
@@ -160,12 +151,8 @@ void deleteBirthday() {
 void searchBirthday() {
   system("clear");
   FILE *file = fopen(FILE_NAME, "r");
-  if (file == NULL) {
-    printf("Can't find file!!!");
-    exit(0);
-  }
+  fileValidation(&file);
 
-  Birthday b;
   char name[NAME_LENGTH];
   int found = 0;
   printf("Enter the name to search: ");
@@ -180,30 +167,45 @@ void searchBirthday() {
   fclose(file);
 
   if (!found) {
-    printf("Data not found!!!\n");
+    printf("Data not found!!!❌\n");
   } 
 
 }
 
 void upcomming() {
+  system("clear");
   time_t t = time(NULL);
   struct tm date = *localtime(&t);
-  Birthday b;
+
   printf("Current date is %02d-%02d-%d\n", date.tm_mday, date.tm_mon + 1, date.tm_year + 1900);
 
   FILE *file = fopen(FILE_NAME, "r");
-  if (file == NULL) {
-      printf("Can't find file!!!");
-      exit(0);
-  }
-
-  int notFound = 1;
+  fileValidation(&file);
+  int found = 0;
   while (fscanf(file, "%s %d %d %d", b.name, &b.day, &b.month, &b.year) != EOF) {
     if (b.month == date.tm_mon + 1) {
       int currentAge = (date.tm_year + 1900) - b.year;
-      notFound = 0;
+      found = 1;
       printf("\n%s is turning %d this month.\n", b.name, currentAge);
     }
   }
-  if (notFound) printf("\nNo upcomming birthday this month.\n");
+  if (!found) printf("\nNo upcomming birthday this month.❌\n");
+}
+
+void todayParty() {
+
+  time_t t = time(NULL);
+  struct tm date = *localtime(&t);
+
+  FILE *file = fopen(FILE_NAME, "r");
+  fileValidation(&file);
+  int found = 0;
+  while (fscanf(file, "%s %d %d %d", b.name, &b.day, &b.month, &b.year) != EOF) {
+    if ((b.day == date.tm_mday) && (b.month == date.tm_mon + 1)) {
+      found = 1;
+      printf("\nToday is %s's birhday. Let's Party!!!🎉🥳\n", b.name);
+    }
+  }
+  if (!found) printf("\nNo Birthday Today😢\n");
+
 }
--- src/main.c
+++ src/main.c
@@ -7,40 +7,42 @@ void updateBirthday();
 void deleteBirthday();
 void searchBirthday();
 void upcomming();
+void todayParty();
 
 int main() {
   int choice;
   system("clear");
   while(1) {
     printf("\nBirthday List Management System\n");
-        printf("1. Add Birthday\n");
-        printf("2. View Birthdays\n");
-        printf("3. Update Birthday\n");
-        printf("4. Delete Birthday\n");
-        printf("5. Search Birthday\n");
-        printf("6. Upcomming Birthday\n");
-        printf("7. Exit\n");
-        printf("Enter your choice: ");
-        // scanf("%d", &choice);
-        if (scanf("%d", &choice) != 1) {
-          printf("Invalid input. Please enter an integer.\n");
+    todayParty();
+    printf("\n1. Add Birthday\n");
+    printf("2. View Birthdays\n");
+    printf("3. Update Birthday\n");
+    printf("4. Delete Birthday\n");
+    printf("5. Search Birthday\n");
+    printf("6. Upcomming Birthday\n");
+    printf("7. Exit\n");
+    printf("Enter your choice: ");
 
-          // Clear the input buffer
-          while (getchar() != '\n');
+    if (scanf("%d", &choice) != 1) {
+      printf("Invalid input. Please enter an integer.❌\n");
 
-          continue;
-      }
-        switch (choice) {
-          case 1: createBirthday(); break;
-          case 2: readBirthday(); break;
-          case 3: updateBirthday(); break;
-          case 4: deleteBirthday(); break;
-          case 5: searchBirthday(); break;
-          case 6: upcomming(); break;
-          case 7: exit(0); break;
-          default: printf("Invalid choice, Please enter the right one!!!!\n");
-        }
-        choice = 0;
+      // Clear the input buffer
+      while (getchar() != '\n');
+
+      continue;
+  }
+    switch (choice) {
+      case 1: createBirthday(); break;
+      case 2: readBirthday(); break;
+      case 3: updateBirthday(); break;
+      case 4: deleteBirthday(); break;
+      case 5: searchBirthday(); break;
+      case 6: upcomming(); break;
+      case 7: exit(0); break;
+      default: printf("Invalid choice, Please enter the right one!!!!❌\n");
+    }
+    choice = 0;
   } 
 
   return 0;
